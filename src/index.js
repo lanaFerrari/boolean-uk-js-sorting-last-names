d7a6f95c6c3f80e0548ddc6a8857d796f3651a10
Lana Ferrari <ferrarirangel@gmail.com>
2021-07-14 16:11:37 +0100

    Deliver 1

--- src/index.js
+++ src/index.js
@@ -4,4 +4,21 @@
 - Have an output that sorts the names alphabetically
 - Have an output that tells you what is the position of your name in the sorted array
 - Have an output that Uppercase's all names
-/*
+*/
+
+const namesList = ["Mason","Marcora","Rico", "Neves", "Ivanov"];
+
+let surnameP = prompt("Please enter your surname");
+console.log(surnameP);
+
+namesList.push(surnameP);
+console.log(namesList);
+
+namesList.sort()
+console.log(namesList);
+
+const namePosition = console.log(namesList.indexOf("Ferrari"));
+console.log(namePosition);
+
+const uppercased = namesList.map(name => name.toUpperCase());
+console.log(uppercased);

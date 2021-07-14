633ee40bfaa180e6fb62ef0d544c1b968a77299b
Lana Ferrari <ferrarirangel@gmail.com>
2021-07-14 18:34:52 +0100

    Delievr 2

--- src/index.js
+++ src/index.js
@@ -1,24 +1,41 @@
-/*
-- Create an array with the following names: Mason, Marcora, Rico, Neves, Ivanov
-- Have a prompt that adds your last name to the array
-- Have an output that sorts the names alphabetically
-- Have an output that tells you what is the position of your name in the sorted array
-- Have an output that Uppercase's all names
-*/
-
+//- Create an array with the following names: Mason, Marcora, Rico, Neves, Ivanov
 const namesList = ["Mason","Marcora","Rico", "Neves", "Ivanov"];
 
+//- Have a prompt that adds your last name to the array
 let surnameP = prompt("Please enter your surname");
 console.log(surnameP);
 
 namesList.push(surnameP);
 console.log(namesList);
 
-namesList.sort()
-console.log(namesList);
 
-const namePosition = console.log(namesList.indexOf("Ferrari"));
-console.log(namePosition);
+//- Have an output that sorts the names alphabetically
+const alphabeticalNames = namesList.sort()
+console.log("sortedLastNames: " , alphabeticalNames);
 
-const uppercased = namesList.map(name => name.toUpperCase());
-console.log(uppercased);
+let positionOfMyLastName = 0;
+
+//- Have an output that tells you what is the position of your name in the sorted array
+
+for (let i = 0; i < namesList.length; i++) {
+        console.log("your name " + namesList[i] + " is in the position", [i]);
+}
+
+// Other solution?
+//const namePosition = namesList.indexOf(surnameP); 
+
+
+//- Have an output that Uppercase's all names
+
+for (let i = 0; i < namesList.length; i++) {
+    const upperCaseNames = namesList[i].toUpperCase();
+        console.log("upperCaseLastNames:  " + upperCaseNames );
+}
+
+
+/*Let's take it a step further and group each sorted name with its upper-cased one in a sub-array,
+ which in its turn will go inside another array to group it all together.
+Output the result into the console.*/
+
+console.log(alphabeticalNames);
+console.log(upperCaseNames);

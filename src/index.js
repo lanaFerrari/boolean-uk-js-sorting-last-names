7d211c2505bf0cacc46d63508b47a4d415baa190
Lana Ferrari <ferrarirangel@gmail.com>
2021-07-14 14:37:12 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/*
+- Create an array with the following names: Mason, Marcora, Rico, Neves, Ivanov
+- Have a prompt that adds your last name to the array
+- Have an output that sorts the names alphabetically
+- Have an output that tells you what is the position of your name in the sorted array
+- Have an output that Uppercase's all names
+/*
